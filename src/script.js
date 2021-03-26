fe8a4e5f5eaaefbdc20c76580176341a02a7f4be
keonlee9420 <keonlee9420@gmail.com>
2021-03-26 18:13:37 +0900

    add style factor control with code revision

--- src/script.js
+++ src/script.js
@@ -1,34 +1,75 @@
 var comb = "1";
-const basedir_F = "./src/F_p233_045_mic1/";
-const basedir_M = "./src/M_p311_094_mic1/";
-// var select = document.getElementById("select");
+var refName = "11111";
+const basedir_F = "./src/F_p233_045/";
+const basedir_M = "./src/M_p311_094/";
+const basedir_cont = "./src/control_r1_F_p323_229_r2_M_p259_284/";
 
-// https://stackoverflow.com/questions/10792163/change-audio-src-with-javascript
-function update_audio(src, gender) {
-   const audioname = gender+"_output_wav";
-   // const sourcename = gender+"_output_wav_source";
-   var audio = document.getElementById(audioname);
-   // var source = document.getElementById(sourcename);
-   // source.src = src;
-   audio.pause();
-   audio.setAttribute('src', src);
-   audio.load();
-   audio.play();
+// https://stackoverflow.com/a/1431113
+String.prototype.replaceAt = function(index, replacement) {
+   return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
-function selectComb(value) {
- const comb = value; 
- if (comb.includes("F")) {
-   const gender = 'F'
-   const filename = "inspect" + comb.replace("F","");
-   const src = basedir_F + filename
-   document.getElementById(gender+"_output_mel").src = src + ".png";
-   update_audio(src + ".wav", gender);
-} else {
-   const gender = 'M'
-   const filename = "inspect" + comb.replace("M","");
-   const src = basedir_M + filename
-   document.getElementById(gender+"_output_mel").src = src + ".png";
-   update_audio(src + ".wav", gender);
- }
+function updateImage(image_elem_id, src) {
+   document.getElementById(image_elem_id).src = src + ".png";
+}
+
+// https://stackoverflow.com/questions/10792163/change-audio-src-with-javascript
+function updateAudio(audio_elem_id, src, play) {
+   var audio = document.getElementById(audio_elem_id);
+   audio.pause();
+   audio.setAttribute('src', src + ".wav");
+   audio.load();
+   if (play) {
+      audio.play();
+   }
+}
+
+function updateContents(src, mel_elem_id, wav_elem_id, play=true) {
+   updateImage(mel_elem_id, src);
+   updateAudio(wav_elem_id, src, play);
+}
+
+function selectComb(comb) {
+   var gender, filename, src;
+   switch(comb.includes("F")) {
+      case true:
+         gender = 'F';
+         filename = "inspect" + comb.replace("F","");
+         src = basedir_F + filename;
+         updateContents(src, gender + "_output_mel", gender + "_output_wav");
+         break;
+      default:
+         gender = 'M';
+         filename = "inspect" + comb.replace("M","");
+         src = basedir_M + filename;
+         updateContents(src, gender + "_output_mel", gender + "_output_wav");
+    }
+}
+
+function updateRef(factorId, refId, play=true){
+   const filename = refName.replaceAt(factorId, refId);
+   refName = filename;
+   const src = basedir_cont + filename;
+   updateContents(src, "cont_output_mel", "cont_output_wav", play);
+}
+
+// TDPES
+function selectRef(ref) {
+   refId = ref.charAt(1);
+   switch(ref.charAt(0)) {
+      case "T":
+         updateRef(0, refId);
+         break;
+      case "D":
+         updateRef(1, refId);
+         break;
+      case "P":
+         updateRef(2, refId);
+         break;
+      case "E":
+         updateRef(3, refId);
+         break;
+      default:
+         updateRef(4, refId);
+   }
 }
